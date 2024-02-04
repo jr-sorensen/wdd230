9f95a13bd3c2505bdb6737fe4cdf4826808a908f
jr-sorensen <sor20009@byui.edu>
2024-02-03 18:08:47 -0800

    other css files

--- chamber/scripts/header-scroll.js
+++ chamber/scripts/header-scroll.js
@@ -2,6 +2,6 @@ document.addEventListener("scroll", function () {
     const h1 = document.querySelector("h1");
     const scrollPos = window.scrollY;
 
-    // Adjust the transform property of h1 based on scroll position
+    
     h1.style.transform = `translateY(${scrollPos * 0.5}px)`;
   });
