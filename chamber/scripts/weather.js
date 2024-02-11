12b86ed282530c68ce50a35b7e33efdd83e1ba26
jr-sorensen <sor20009@byui.edu>
2024-02-10 18:17:38 -0800

    update color

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,5 +1,5 @@
 
-const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=34.686787&longitude=-73.98529171943665&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=34.686787&longitude=118.154160&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
 
 
 const celsiusToFahrenheit = (celsius) => (celsius * 9/5) + 32;
