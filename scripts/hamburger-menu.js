de54bf9dd5c905d66fd69ffc743458a1176ecae3
jr-sorensen <sor20009@byui.edu>
2024-02-15 19:06:39 -0800

    fixes

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hamburgerIcon = document.querySelector('.hamburger-menu');
-    const mobileMenu = document.querySelector('nav ul');
+    const mobileMenu = document.querySelector('.main-menu ul');
+    const exitButton = document.querySelector('.exit-button');
 
-    hamburgerIcon.addEventListener('click', function () {
+    hamburgerIcon.addEventListener('click', toggleMobileMenu);
+    exitButton.addEventListener('click', closeMobileMenu);
+
+    function toggleMobileMenu() {
         mobileMenu.classList.toggle('show-menu');
         const isOpen = mobileMenu.classList.contains('show-menu');
         hamburgerIcon.innerHTML = isOpen ? '&#10005;' : '&#8801;';
-    });
+
+        // Show/hide the exit button based on menu visibility
+        exitButton.style.display = isOpen ? 'block' : 'none';
+    }
+
+    function closeMobileMenu() {
+        mobileMenu.classList.remove('show-menu');
+        hamburgerIcon.innerHTML = '&#8801;';
+        exitButton.style.display = 'none';
+    }
 });
