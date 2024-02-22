f1344c89306369b73cbec4096b24a48608b1cabe
jr-sorensen <sor20009@byui.edu>
2024-02-21 21:35:48 -0800

    visitors

---
+++ scripts/storage.js
@@ -0,0 +1,7 @@
+let pageVisitCounter = localStorage.getItem('pageVisitCounter') || 0;
+
+        pageVisitCounter++;
+
+        localStorage.setItem('pageVisitCounter', pageVisitCounter);
+
+        document.getElementById('visitCounter').innerText = pageVisitCounter;
