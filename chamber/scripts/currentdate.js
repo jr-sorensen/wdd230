bdcba193bda76f7e1c2259e685614e02080ac4c7
jr-sorensen <sor20009@byui.edu>
2024-02-23 23:11:39 -0800

    add

--- chamber/scripts/currentdate.js
+++ chamber/scripts/currentdate.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
 
-    const currentDate = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedCurrentDate = currentDate.toLocaleDateString('en-US', options);
+//     const currentDate = new Date();
+//     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+//     const formattedCurrentDate = currentDate.toLocaleDateString('en-US', options);
 
-    document.getElementById("currentDate").innerHTML = `${formattedCurrentDate} and Beyond`;
+//     document.getElementById("currentDate").innerHTML = `${formattedCurrentDate} and Beyond`;
 
 
-    const lastModified = new Date(document.lastModified);
-    const formattedLastModified = lastModified.toLocaleString('en-US', options);
+//     const lastModified = new Date(document.lastModified);
+//     const formattedLastModified = lastModified.toLocaleString('en-US', options);
 
-    document.getElementById("lastModified").innerHTML = `Last Modified: ${formattedLastModified}`;
-});
+//     document.getElementById("lastModified").innerHTML = `Last Modified: ${formattedLastModified}`;
+// });
 
 document.addEventListener('DOMContentLoaded', function () {
     
