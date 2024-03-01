01effdb004749d024e1b7ad1ef7db498ca173890
jr-sorensen <sor20009@byui.edu>
2024-03-01 14:31:14 -0800

    form

--- chamber/scripts/modifidedate.js
+++ chamber/scripts/modifidedate.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
     var lastModifiedDate = document.lastModified;
-
-    
     var modificationDateElement = document.getElementById('modificationDate');
     if (modificationDateElement) {
         modificationDateElement.textContent = 'Page Last Modified: ' + lastModifiedDate;
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var formLoadDateTimeField = document.getElementById('formLoadDateTime');
+    var submissionTimeSpan = document.getElementById('submissionTime');
+
+    if (formLoadDateTimeField && submissionTimeSpan) {
+        formLoadDateTimeField.value = Date.now();
+
+        var submissionTime = new Date().toLocaleString();
+        submissionTimeSpan.textContent = submissionTime;
     }
 });
